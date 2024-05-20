e2fe339efb8ad5e81ebae8c463ddd8fabcdd3540
MarionLnd <maye43@gmail.com>
2024-05-20 18:33:32 +0200

    feat: update and styling Solution page

--- src/views/SolutionsView.vue
+++ src/views/SolutionsView.vue
@@ -1,63 +1,101 @@
 <script setup lang="ts"></script>
 
 <template>
-  <div>
-    Nos solutions
-    <div>
-      <h1>Pour professionnels</h1>
-      <p>
-        Que vous soyez une copropriété, un commerce, des bureaux ou tout autre professionnel, notre
-        entreprise est là pour vous aider en cas de problème électrique. Nous savons que les pannes
-        électriques peuvent vous faire perdre du temps et de l’argent, c’est pourquoi nous nous
-        engageons à intervenir dans les plus brefs délais en fonction de notre disponibilité.
-      </p>
-      <p>
-        Notre première étape consiste à intervenir directement sur site pour anticiper les travaux à
-        initier. Cette étape est essentielle pour des installations optimales dans les espaces
-        dédiés. Ensuite, grâce à notre audit technique, nous pouvons proposer une solution efficace,
-        économique et durable pour résoudre ou répondre à vos attentes.
-      </p>
-      <p>
-        Notre équipe de techniciens expérimentés et qualifiés est à votre disposition pour tous vos
-        besoins en matière d’installation électrique. Nous sommes équipés de tous les outils
-        nécessaires pour garantir des interventions rapides et efficaces. Chez
-        <span class="text-blue-500 font-semibold">POLYTECH -E- SERVICES</span>
-        nous sommes engagés à fournir des solutions électriques durables et respectueuses de
-        l’environnement.
-      </p>
+  <div class="container sm:my-7">
+    <h1 class="uppercase text-4xl font-bold text-center text-blue-500">Nos solutions</h1>
+    <div class="flex justify-center gap-x-12 sm:mt-10 divide-x">
+      <div class="basis-8/12 text-justify">
+        <h2 class="uppercase text-xl font-bold text-blue-800 sm:mb-3">Pour professionnels</h2>
+        <p>
+          Que vous soyez une copropriété, un commerce, des bureaux ou tout autre professionnel,
+          notre entreprise est là pour vous aider en cas de problème électrique. Nous savons que les
+          pannes électriques peuvent vous faire perdre du temps et de l’argent, c’est pourquoi nous
+          nous engageons à intervenir dans les plus brefs délais en fonction de notre disponibilité.
+        </p>
+        <p>
+          Notre première étape consiste à intervenir directement sur site pour anticiper les travaux
+          à initier. Cette étape est essentielle pour des installations optimales dans les espaces
+          dédiés. Ensuite, grâce à notre audit technique, nous pouvons proposer une solution
+          efficace, économique et durable pour résoudre ou répondre à vos attentes.
+        </p>
+        <p>
+          Notre équipe de techniciens expérimentés et qualifiés est à votre disposition pour tous
+          vos besoins en matière d’installation électrique. Nous sommes équipés de tous les outils
+          nécessaires pour garantir des interventions rapides et efficaces.
+        </p>
+        <br />
+        <p class="italic">
+          Chez
+          <span class="text-blue-500 font-semibold">POLYTECH -E- SERVICES</span>
+          nous sommes engagés à fournir des solutions électriques durables et respectueuses de
+          l’environnement.
+        </p>
+        <br />
+        <div>
+          <h3 class="uppercase font-bold">Nos solutions</h3>
+          <ul class="list-disc list-inside sm:ml-4">
+            <li>Système éclairage (éco détection, LED, …)</li>
+            <li>
+              Installation purement électrique (création de prise mono ou triphasé, séparation
+              éclairage, …)
+            </li>
+            <li>
+              Travaux purement électrique (réhabilitation et mise en sécurité électrique, recherche
+              de panne, diagnostic d’installation, …)
+            </li>
+            <li>
+              Installation et travaux informatique/réseaux (câblage réseau, mise au propre et
+              changement de baie informatique, dépannage sur câble réseau, …)
+            </li>
+            <li>Installation de borne de recharge de véhicule électrique.</li>
+          </ul>
+        </div>
+      </div>
+
+      <div class="text-justify px-12">
+        <h2 class="uppercase text-xl font-bold text-blue-800 sm:mb-3">Pour les particuliers</h2>
+        <ul class="list-disc list-inside">
+          <li>Réhabilitation électrique</li>
+          <li>Dépannage et recherche de panne</li>
+          <li>Installation neuve ou rénovation</li>
+          <li>Changement de tableau électrique</li>
+          <li>Mise à la terre</li>
+          <li>Domotique</li>
+          <li>Coffret de communication</li>
+          <li>Borne de recharge pour V.E</li>
+        </ul>
+        <p>Quelques tarifs pour particuliers :</p>
+        <ul class="list-disc list-inside">
+          <li>Changement de tableau électrique et mise aux normes :</li>
+          <ul class="list-disc list-inside ml-6">
+            <li>Tableau 2 rangées à partir de 1080 euros TTC</li>
+            <li>Tableau 3 rangées à partir de 1220 euros TTC</li>
+            <li>Tableau 4 rangées à partir de 1400 euros TTC</li>
+          </ul>
+          <li>Rénovation électrique complète :</li>
+          <ul class="list-disc list-inside ml-6">
+            <li>A partir de 95 euros/m2 TTC pour une installation avant doublage</li>
+            <li>
+              A partir de 89 euros/m2 TTC pour une installation saillie/apparent (sous moulure)
+            </li>
+            <li>A partir de 140 euros/m2 TTC pour une installation encastrée (sous saignée)</li>
+          </ul>
+          <li>Installation borne de recharge de véhicule électriques :</li>
+          <ul class="list-disc list-inside ml-6">
+            <li>A partir de 990 euros TTC.</li>
+          </ul>
+        </ul>
+      </div>
     </div>
-    <div>
-      <h3>Nos solutions</h3>
-      <h4>Pour les professionnels</h4>
-      <ul>
-        <li>Système éclairage (éco détection, LED, …)</li>
-        <li>
-          Installation purement électrique (création de prise mono ou triphasé, séparation
-          éclairage, …)
-        </li>
-        <li>
-          Travaux purement électrique (réhabilitation et mise en sécurité électrique, recherche de
-          panne, diagnostic d’installation, …)
-        </li>
-        <li>
-          Installation et travaux informatique/réseaux (câblage réseau, mise au propre et changement
-          de baie informatique, dépannage sur câble réseau, …)
-        </li>
-        <li>Installation de borne de recharge de véhicule électrique.</li>
-      </ul>
-      <br />
-      <h4>Pour les particuliers</h4>
-      <ul>
-        <li>Réhabilitation électrique</li>
-        <li>Dépannage et recherche de panne</li>
-        <li>Installation neuve ou rénovation</li>
-        <li>Changement de tableau électrique</li>
-        <li>Mise à la terre</li>
-        <li>Domotique</li>
-        <li>Coffret de communication</li>
-        <li>Borne de recharge pour V.E</li>
-      </ul>
-    </div>
+
+    <p class="sm:mt-7">
+      <span class="font-bold">Des projets ?</span> Contactez-nous dès maintenant et soumettez-nous
+      votre demande via le
+      <a class="underline font-bold text-blue-500 hover:text-blue-800 hover:cursor-pointer"
+        >formulaire de contact prévu</a
+      >. Nous vous répondrons dans les meilleurs délais afin de vous proposer les solutions les plus
+      adaptées, en fonction de votre localisation.
+    </p>
   </div>
 </template>
 
